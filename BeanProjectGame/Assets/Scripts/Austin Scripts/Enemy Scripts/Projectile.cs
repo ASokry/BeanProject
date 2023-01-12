aca1c091d4cd98574d9764cd2d3af348468f3226
AustinGlueck <austin.m.glueck@gmail.com>
2023-01-12 11:06:55 -0800

    Hive projectile implementation impovements
    Hooked up animations properly for the hive projectile and gave it the potential to damage the player.

--- BeanProjectGame/Assets/Scripts/Austin Scripts/Enemy Scripts/Projectile.cs
+++ BeanProjectGame/Assets/Scripts/Austin Scripts/Enemy Scripts/Projectile.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public bool mayMoveForward;
     public float speed;
+    public int damage;
+    private CharacterMotion characterMotion;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    public void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        characterMotion = player.GetComponent<CharacterMotion>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-transform.right * speed * Time.deltaTime);
+        if (mayMoveForward)
+        {
+            transform.Translate(-transform.right * speed * Time.deltaTime);
+        }
+
+    }
+
+    public void BugEmerged()
+    {
+        mayMoveForward = true;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            print("Dammage");
+            characterMotion.TakeDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }

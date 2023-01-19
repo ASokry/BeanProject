98a8868bfc5b198e8c660962abbf7dd0ee78db54
ASokry <anthonysokry11@gmail.com>
2023-01-18 18:43:15 -0800

    inventory Upgrade error investgation

--- BeanProjectGame/Assets/Scripts/InventoryTetris/Inventory/InventoryTetris.cs
+++ BeanProjectGame/Assets/Scripts/InventoryTetris/Inventory/InventoryTetris.cs
@@ -56,13 +56,13 @@ public class InventoryTetris : MonoBehaviour {
 
     private void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.U) && InventoryGridManager.Instance.GetCurrentState() != InventoryGridManager.InventoryState.Upgrading)
+        if (Input.GetKeyDown(KeyCode.U) && InventoryGridManager.Instance.GetCurrentState() != InventoryGridManager.InventoryState.Upgrading)
         {
             InventoryGridManager.Instance.SetCurrentState(InventoryGridManager.InventoryState.Upgrading);
             InventoryGridManager.Instance.SetStartingUpgradePoints(3);
             ShowUpgradeableTiles();
         }
-        ClickToExpandGrid();*/
+        ClickToExpandGrid();
     }
 
     public class GridObject {
